42de11015c1a6271f788a5fa05e28eed44f76fe6
Joaquin Huaman Pino <jhuaman@evolutionsoluciones.com>
2024-05-17 20:00:01 -0500

    core

--- utils/mixins.py
+++ utils/mixins.py
@@ -1,8 +1,10 @@
-from django.db import models
 import datetime
+from django.db import models
+
 
 class TimestampMixin(models.Model):
     created_at = models.DateTimeField(default=datetime.datetime.now)
     updated_at = models.DateTimeField(default=datetime.datetime.now)
+
     class Meta:
         abstract = True
